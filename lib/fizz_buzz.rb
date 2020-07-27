3d54d5acce74f6ad273ee5984d33f7af97882688
munemo <john.chimbani@outlook.com>
2020-07-27 15:23:50 +0200

    Fifth fizz_buz kata commit

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,15 @@
 def fizz_buzz(number)
-  if number % 3 == 0
-    'fizz'
-  else
-    number
-  end 
+if has_zero_remainder?(number, 15)
+  'fizz buzz'
+elsif has_zero_remainder?(number, 5)
+  'buzz'
+elsif has_zero_remainder?(number, 3)
+  'fizz'
+else
+  number
+end
+end
+
+def has_zero_remainder?(number, divider)
+number % divider == 0
 end
